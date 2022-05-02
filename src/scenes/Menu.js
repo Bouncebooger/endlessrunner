aab3da484c153e89a42abeade2c82184cd16e8ed
Marlene Lopez <91994568+dokimar@users.noreply.github.com>
2022-05-02 15:36:28 -0700

    update with instruction and credits line

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -11,13 +11,52 @@ class Menu extends Phaser.Scene {
    }
    
    create(){
+      // background
       this.background = this.add.tileSprite(0, 0, 300, 256, 'bg').setOrigin(0,0).setScale(3,3.5);
       
+      // button
       this.button = this.add.image(game.canvas.width/2, game.canvas.height/2, 'button', 1);
       this.button.setInteractive();
+      
+      // title
       const title = this.add.text(game.canvas.width/2, game.canvas.height/4, "Slug Fall").setOrigin(0.5,0);
       title.setFontSize(100);
       this.start = false;
+      
+      
+      // other text on screen
+      //
+      // text configuration
+      let textConfig = {
+         fontFamily: 'Courier',
+         fontSize: '28px',
+         color: '#FF7F50',
+         align: 'center',
+         padding: {
+             top: 5,
+             bototm: 5,
+         },
+         stroke: '#000000',
+         strokeThickness: 5,
+         fixedWidth: 0
+      }
+      // setting the center of text
+      let centerX = game.config.width/2;
+      let centerY = game.config.height/2;
+      let textSpacer = 48;
+
+      // game instructions 
+      this.add.text(centerX, centerY + 2 * textSpacer, 'Move with the Arrow Keys (←)(→)', textConfig).setOrigin(0.5);
+      this.add.text(centerX, centerY + 3 * textSpacer, 'Press (Q) to Break Through Webs', textConfig).setOrigin(0.5);
+      // Goal
+      textConfig.color = '#FFFF66';
+      this.add.text(centerX, centerY + 4 * textSpacer, '>> Avoid the Bird <<', textConfig).setOrigin(0.5);
+      // credits line
+      textConfig.color = '#5F9EA0';
+      textConfig.fontSize= '14px';
+      this.add.text(centerX, centerY + 7 * textSpacer, 'Game by Skyler Haataja, Marlene Lopez, and Daniel Wild', textConfig).setOrigin(0.5);
+
+
       
    }
    update(){

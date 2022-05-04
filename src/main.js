36001f1c91601d39252a1ffacce75391be9f87c7
dwild <dwild@ucsc.edu>
2022-05-04 14:54:29 -0700

    bad Creative Tilt lmao

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+//Creative Tilt
+// Our game implements a rudimentary velocity as 
+// its main mechanic.
+// In order to do this,we had to make the world 
+// move instead of the player. (move objects up) 
+// The matter physics collision has a few quirks, 
+// one was that the player, upon collision with a
+// platform would result in the player clipping 
+// into said platform. 
+// This was fixed by halting the movement of 
+// everything but the player upon collision.
+// We also had to deal with scene pausing and time 
+// events.
+// The fact that we used matterphysics was going 
+// beyond the class examples. 
+// We learned much about how groups, bodies, and
+// collision categories COULD work, which should
+// prove useful for the next game. 
+//
+
 let config = {
     type: Phaser.CANVAS,
      width: 900,
